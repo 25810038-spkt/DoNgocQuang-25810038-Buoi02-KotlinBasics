5d4d04af0a533b1a0298b48bc5967e9bda528639
QuangDN <info.dongocquang@gmail.com>
2026-09-23 21:20:39 +0700

    Buoi02 Bai01 tinh tien mua hang

--- Bai01_TinhTienMuaHang.kts
+++ Bai01_TinhTienMuaHang.kts
@@ -2,7 +2,7 @@ fun TinhTienMuaHang() {
     val soluong: Int = 2
     val dongia: Double = 2.0
     val soluongCasted: Double = soluong.toDouble()
-    var tienhang = (soluong * dongia)
+    var tienhang = (soluongCasted * dongia)
     tienhang += (tienhang * 8) / 100
     print("$tienhang VND")
 }
