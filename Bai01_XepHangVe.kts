7bfaf2e712b69ba133f77c888b1d8a852e231cf2
QuangDN <info.dongocquang@gmail.com>
2026-09-25 19:41:03 +0700

    Buoi03 Bai01 Xếp hạng vé xem phim theo độ tuổi

---
+++ Bai01_XepHangVe.kts
@@ -0,0 +1,15 @@
+fun XepHangVe() {
+    val tuoi = 26
+    if (tuoi < 18) {
+        val loaiVe = "Trẻ em"
+        println(loaiVe)
+    } else if (tuoi < 28) {
+        val loaiVe = "Người lớn"
+        println(loaiVe)
+    } else {
+        val loaiVe = "Cao tuổi"
+        println(loaiVe)
+    }
+}
+
+XepHangVe()
