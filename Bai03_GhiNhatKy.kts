cea9d65653b4226028140b85c961b1e96c0db9c0
QuangDN <info.dongocquang@gmail.com>
2026-09-25 20:01:59 +0700

    Buoi03 Bai03 Ghi nhật ký hoạt động

---
+++ Bai03_GhiNhatKy.kts
@@ -0,0 +1,9 @@
+fun ghiNhatKy1(motahanhdong: String): Unit {
+    println(motahanhdong)
+}
+fun ghiNhatKy2(motahanhdong: String) {
+    println(motahanhdong)
+}
+
+ghiNhatKy1("Action")
+ghiNhatKy2("Action")
