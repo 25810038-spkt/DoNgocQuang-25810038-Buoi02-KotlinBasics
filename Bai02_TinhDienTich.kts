651b0c7a40c53ff3d78e553fcafadbce6dbfdd48
QuangDN <info.dongocquang@gmail.com>
2026-09-25 19:46:04 +0700

    Buoi03 Bai02 Tính diện tích hình chữ nhật

---
+++ Bai02_TinhDienTich.kts
@@ -0,0 +1,6 @@
+fun tinhDienTich(chieudai: Double, chieurong: Double): Double {
+    return chieudai * chieurong
+}
+
+println(tinhDienTich(chieudai = 10.0, chieurong = 20.0))
+println(tinhDienTich(chieudai = 20.0, chieurong = 40.0))
