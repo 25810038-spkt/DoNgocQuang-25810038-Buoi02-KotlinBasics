c1866e444aed2626f4cf58005527b1ad1008115a
QuangDN <info.dongocquang@gmail.com>
2026-09-25 20:12:40 +0700

    Buoi03 Bai05 Định dạng địa chỉ giao hàng

---
+++ Bai05_DinhDangDiaChi.kts
@@ -0,0 +1,9 @@
+fun dinhDangDiaChi(par1: Int, par2: Int, par3: Int = 3, par4: Int = 4, par5: Int = 5) {
+    println(par1)
+    println(par2)
+    println(par3)
+    println(par4)
+    println(par5)
+}
+
+dinhDangDiaChi(1, 2, par3 = 33, par4 = 44, par5 = 55)
